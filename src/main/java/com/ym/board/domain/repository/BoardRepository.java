fdb548d927d59bb2f3e0ae4ea2ca2275bf9663e5
YeongMin Park <py980627@skuniv.ac.kr>
2023-03-01 00:41:38 +0900

    new commit

--- src/main/java/com/ym/board/domain/repository/BoardRepository.java
+++ src/main/java/com/ym/board/domain/repository/BoardRepository.java
@@ -4,7 +4,8 @@ import com.ym.board.domain.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-
+//boardRepository
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
     List<BoardEntity> findByTitleContaining(String keyword);
+
 }
